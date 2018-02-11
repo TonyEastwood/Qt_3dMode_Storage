f550f88f74be498c6a0f7a22da8d0e29c62a5083
Tony Eastwood <erlinmaquer@gmail.com>
2018-02-11 03:10:24 +0200

    reload all

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "form_log_in.h"
-#include <QApplication>
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Form_Log_In w;              //create Log In form to log in in database
-    w.show();                   //show Log In form
-    return a.exec();
-}
